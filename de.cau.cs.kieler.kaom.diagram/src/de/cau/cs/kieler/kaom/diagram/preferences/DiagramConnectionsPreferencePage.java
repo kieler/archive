45b5b984150b1000507804de1528f9836f27a709
Miro Spoenemann <msp@informatik.uni-kiel.de>
2013-07-08 10:28:11 +0200

    copied kaom plugins from pragmatics repository

---
+++ de.cau.cs.kieler.kaom.diagram/src/de/cau/cs/kieler/kaom/diagram/preferences/DiagramConnectionsPreferencePage.java
@@ -0,0 +1,18 @@
+package de.cau.cs.kieler.kaom.diagram.preferences;
+
+import org.eclipse.gmf.runtime.diagram.ui.preferences.ConnectionsPreferencePage;
+
+import de.cau.cs.kieler.kaom.diagram.part.KaomDiagramEditorPlugin;
+
+/**
+ * @generated
+ */
+public class DiagramConnectionsPreferencePage extends ConnectionsPreferencePage {
+
+    /**
+     * @generated
+     */
+    public DiagramConnectionsPreferencePage() {
+        setPreferenceStore(KaomDiagramEditorPlugin.getInstance().getPreferenceStore());
+    }
+}
